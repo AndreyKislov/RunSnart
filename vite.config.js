564de8ba124cdd94c814a0c2297a37373455aa8e
Andrey Kislov <109723051+AndreyKislov@users.noreply.github.com>
2024-09-24 21:09:34 +0300

    Add consultation block with form and add media for device sizes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import {defineConfig} from "vite";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig(
     {
         css: {
             devSourcemap: true,
+            postcss:{
+                plugins:[
+                    autoprefixer({})
+                ]
+            }
         },
     }
 );
